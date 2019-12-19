daa35aaf46ce25ceb6ef2d2acac624ce5400d064
helian2 <helian2@yy.com>
2019-12-19 11:20:45 +0800

    add train process

--- jpgtogif.py
+++ jpgtogif.py
@@ -4,4 +4,7 @@ for i in [9,19,29,39,49,59,69]:
     print("./result_%d.jpg" % i)
     im = imageio.imread("./result_%d.jpg" % i)
     frames.append(im)
-imageio.mimsave("result.gif",frames,'GIF',duration = 0.1)
+im = imageio.imread("./result_69.jpg" )
+frames.append(im)
+
+imageio.mimsave("result.gif",frames,'GIF',duration = 0.3)
